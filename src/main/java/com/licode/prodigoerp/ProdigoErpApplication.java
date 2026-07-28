f03f458d32d46bff4f37617f44f4b65fcf19044a
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-07-27 20:42:50 -0400

    initial commit

---
+++ src/main/java/com/licode/prodigoerp/ProdigoErpApplication.java
@@ -0,0 +1,13 @@
+package com.licode.prodigoerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdigoErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdigoErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/licode/prodigoerp/ProdigoErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licode.prodigoerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdigoErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
