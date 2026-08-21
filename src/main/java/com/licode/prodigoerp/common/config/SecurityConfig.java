32f9b4de5b7b7b043a6db153a7bdac2bef07b8c3
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-08-20 21:49:26 -0400

    refactor(Hex): Move SecurityUtils to common.security package
    Relocates `SecurityUtils` from `common_old.security` to `common.security` and updates its package/imports to use the new security DTO and exception namespaces. Also updates `SecurityConfig` to import `JwtAuthFilter` from `common.security`, keeping security wiring aligned with the package migration away from `common_old`.

--- src/main/java/com/licode/prodigoerp/common/config/SecurityConfig.java
+++ src/main/java/com/licode/prodigoerp/common/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.licode.prodigoerp.common.config;
 
-import com.licode.prodigoerp.common_old.security.JwtAuthFilter;
+import com.licode.prodigoerp.common.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
